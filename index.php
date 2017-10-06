ddcda9a829aad6f9e122e720dc5d549da8b8d980
draikan <31989340+draikan@users.noreply.github.com>
2017-10-06 14:55:15 +0200

    modif index + ajout du fichier function

--- index.php
+++ index.php
@@ -1,54 +1,36 @@
-<<<<<<< HEAD
-<?php session_start();//connexion bdd	
-		
-	try
-	{
-		$bdd = new PDO("mysql:host=localhost;dbname=rs;charset=utf8","root","");
-	
-	}
-		catch(Exeption $e)
-	{
-		die("erreur de connection");
-	}
-	?>
 <?php
+    session_start();
+    
+    require "core/functions.php";
+    require "models/connect.php";
 
-define('WEBROOT',dirname(__FILE__));
-define('BASE_URL',dirname($_SERVER['SCRIPT_NAME']));
-define('ROOT',dirname(WEBROOT));
-define('DS',DIRECTORY_SEPARATOR);
-define('CORE',ROOT.DS.'core');
+    define('WEBROOT', dirname(__FILE__));
+    define('BASE_URL', dirname($_SERVER['SCRIPT_NAME']));
+    define('ROOT', dirname(WEBROOT));
+    define('DS', DIRECTORY_SEPARATOR);
+    define('CORE',ROOT.DS.'core');
 
+      if(!isset($_GET['p']) || $_GET['p'] == "")
+      {
+          $page = 'accueil';
+      }
+      else
+      {
+          if(!file_exists("controllers/".$_GET['p'].".php"))
+          {
+              $page = '404';
+          }
+          else
+          {
+            $page = $_GET['p'];
+          }
+      }
 
+    
 
-	if (!isset($_GET['page']) || $_GET['page'] == "")
-	{
-        echo"aa";
-		$page = "accueil";
-	}
-	else
-	{
-		if (!file_exists("contenu/" . $_GET['page'] . ".php"))
-		{
-			$page = "404";
-		}
-		else
-        {
-            $page=$_GET['page'];
-        }
-    }
-ob_start();
-include "contenu/".$page.".php";
-$contenu = ob_get_contents();
-ob_end_clean();	
-
-//require 'includes/config.php';
-require 'layout.php';
-
-
+    ob_start();//permet de ne plus renvoyer de contenu au navigateur
+    require "controllers/".$page.".php";
+    $content = ob_get_contents();//permet de recuperer le contenu executer depuis ob_start
+    ob_end_clean();
+    require "layout.php";
 ?>
-=======
-<?php
-
-require "/Views/home.php";
->>>>>>> caa67980643649a31ba47f87a48da8c4fdd54a11
