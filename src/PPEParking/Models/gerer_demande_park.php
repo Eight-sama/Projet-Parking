d5d88abff706e350e7af5785cf0c407d5bc7322d
Luc-Leveque <31991589+Luc-Leveque@users.noreply.github.com>
2017-10-24 16:50:20 +0200

    rajout de la durée

--- src/PPEParking/Models/gerer_demande_park.php
+++ src/PPEParking/Models/gerer_demande_park.php
@@ -12,6 +12,7 @@ function afficher_demande(){
 
 
 
+
 function valider_reserv($id){
     
     global $bdd;
@@ -20,11 +21,29 @@ function valider_reserv($id){
     
     $req = "SELECT * FROM occuper WHERE id_u = 'null'  ";
     $requete = $bdd->query($req);
-
+    
+    $date_deb = date("Y-m-d H:i:s") ;  
+    
+    $date_explosee = explode(" ", $date_deb);
+    $jmd = $date_explosee[0];
+    $his = $date_explosee[1];
+    
+    $date_explosee = explode("-", $date_deb);
+    $year = $date_explosee[0];
+    $mois= $date_explosee[1];
+    $jour= $date_explosee[2];
+    
+    
+    $date_fin = mktime(0,0,0,$mois,$jour+7,$year);//durée fix a une semaine
+    $date_fin = date("Y-m-d H:i:s" , $date_fin);
+    
+    
     if($requete->fetch()){
         
-        $requete2 = $bdd->prepare("Update occuper set id_u = :id WHERE id_u = 'null' limit 1 ");
+        $requete2 = $bdd->prepare("Update occuper set id_u = :id , date_deb = :date_deb , date_fin= :date_fin  WHERE id_u = 'null' limit 1 ");
         $requete2 ->bindValue(":id",$id,PDO::PARAM_INT);
+        $requete2 ->bindValue(":date_deb",$date_deb,PDO::PARAM_INT);
+        $requete2 ->bindValue(":date_fin",$date_fin,PDO::PARAM_INT);
         $requete2->execute();
         
         $requete5 = $bdd->prepare("Update user set lvl = 4 where id_u = :id ");
