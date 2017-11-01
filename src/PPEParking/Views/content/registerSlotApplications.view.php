675f6f61e1df3739bdca237c37560ec91861be1c
Eight-sama <exoth.ousha@gmail.com>
2017-11-01 13:12:30 +0100

    Update refresh status slot's button

--- src/PPEParking/Views/content/registerSlotApplications.view.php
+++ src/PPEParking/Views/content/registerSlotApplications.view.php
@@ -41,7 +41,7 @@
                 <li role="presentation"><a class="btn btn-primary" href="#onmodif">Voir les demandes en cours</a></li>
                 <li role="presentation"><a class="btn btn-success" href="#accepted">Voir les demandes acceptées</a></li>
                 <li role="presentation"><a class="btn btn-danger" href="#refused">Voir les demandes refusées</a></li>
-                <li role="presentation"><a class="fa fa-refresh btn btn-default " href="#update_status_slot">Mettre à jour la liste</a></li>
+                <li role="presentation"><a class="btn btn-warning" href="#update_status_slot"><i class="fa fa-refresh"></i> Mettre à jour la liste</a></li>
             </ul>
         </div>
         <br>
