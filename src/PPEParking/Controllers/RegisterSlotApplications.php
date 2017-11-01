65bfb88e32a9cf5747aac887f0b3c86645c34287
Eight-sama <exoth.ousha@gmail.com>
2017-11-01 13:27:55 +0100

    Fixed accept slot first query

--- src/PPEParking/Controllers/RegisterSlotApplications.php
+++ src/PPEParking/Controllers/RegisterSlotApplications.php
@@ -19,7 +19,7 @@ class RegisterSlotApplications extends Functions
             $request = $db->query("DELETE * FROM user WHERE id_u = '".$_GET['id']."'");
             header('Location: '.BASE_URL.'/index.php?page=registerSlotApplications');
         } elseif(isset($_GET['acceptSlot'])){
-            $request = $db->query("UPDATE reserve SET etat = 1 WHERE id_u = '".$_GET['id']."' AND id_s = '".$_GET['id_slot']."' AND id_r IN ('".$_GET['id_r']."')");
+            $request = $db->query("UPDATE reserve SET etat = 1 WHERE id_u = '".$_GET['id']."' AND id_s = '".$_GET['id_slot']."' AND id_r = '".$_GET['id_r']."'");
             $request = $db->query("DELETE FROM reserve WHERE id_u = '".$_GET['id']."' AND etat = 0");
             $request = $db->query("UPDATE slot SET state_s = 1 WHERE id_s = '".$_GET['id_slot']."'");
             header('Location: '.BASE_URL.'/index.php?page=registerSlotApplications');
